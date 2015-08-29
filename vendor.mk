1c00572908d3c6ecb3a07b885cbe874c4872361e
Ron DuPlain <ron.duplain@gmail.com>
2015-08-29 13:24:29 -0400

    Use latest nixd, update on version spec change.

--- vendor.mk
+++ vendor.mk
@@ -1,16 +1,18 @@
+__FILE__ := $(lastword $(MAKEFILE_LIST))
 DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 include $(DIR)/path.mk
 
-NIXD_VERSION := 7bafe5c3a49c784a5b2e6a999d1f28d719843b94
-NIXD_SHA1SUM := 22981df71e81f2780ded6874a0dbec78ad18710d
+NIXD_VERSION := fa4fc39e7fd8c9a9183684e349c81931326d7523
+NIXD_SHA1SUM := b30e0e2927fef8b492223c1daf35e9f818584065
 
 VENDOR := $(PROJECT_ROOT)/.nixd
 nixd := $(VENDOR)/bin/nixd
 
 export VENDOR
 
-$(nixd):
+$(nixd): $(__FILE__)
+	@rm -f $@
 	@$(VENDOR)/bin/nixd-bootstrap $(nixd) $(NIXD_VERSION) sha1 $(NIXD_SHA1SUM)
 
 vendor-%: $(nixd)
