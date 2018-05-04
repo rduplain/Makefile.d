1d07399178da3b46e95f17808843f60b7489a8f5
Ron DuPlain <ron.duplain@gmail.com>
2018-05-04 19:46:57 -0400

    Use nixd v0.4.

--- vendor.mk
+++ vendor.mk
@@ -3,8 +3,8 @@ DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 include $(DIR)/path.mk
 
-NIXD_VERSION := fa4fc39e7fd8c9a9183684e349c81931326d7523
-NIXD_SHA1SUM := b30e0e2927fef8b492223c1daf35e9f818584065
+NIXD_VERSION := 99053ca3d12b82975720ae56245e25983cac3e91
+NIXD_SHA256 := 1bc4594ab25f71673fc958997852243e5e137863f90de16d8a21a42a999e7b6a
 
 VENDOR := $(PROJECT_ROOT)/.nixd
 nixd := $(VENDOR)/bin/nixd
@@ -13,7 +13,7 @@ export VENDOR
 
 $(nixd): $(__FILE__)
 	@rm -f $@
-	@$(VENDOR)/bin/nixd-bootstrap $(nixd) $(NIXD_VERSION) sha1 $(NIXD_SHA1SUM)
+	@$(VENDOR)/bin/nixd-bootstrap $(nixd) $(NIXD_VERSION) sha256 $(NIXD_SHA256)
 
 vendor-%: $(nixd)
 	@$(nixd) install $*
