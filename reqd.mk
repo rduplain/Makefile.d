3badd908cc63486eb83233f64a74afdb7718ef19
Ron DuPlain <ron.duplain@gmail.com>
2018-08-19 01:58:54 -0400

    Use reqd v2.0.

--- reqd.mk
+++ reqd.mk
@@ -1,19 +1,26 @@
 __FILE__ := $(lastword $(MAKEFILE_LIST))
 DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
+include $(DIR)/command.mk
 include $(DIR)/path.mk
 
-NIXD_VERSION := v1.0
-NIXD_SHA256 := 2cfe294fa454d67f31cf71b150aa969b81938e3eb854d45fd2251761a2401be3
+REQD_VERSION := v2.0
+REQD_SHA256 := dca4495f40c1b5c23b1f6edf7405ee56d4c6ff224c972de002ec852b48dd2797
 
-VENDOR := $(PROJECT_ROOT)/.nixd
-nixd := $(VENDOR)/bin/nixd
+GITHUB_RAW := raw.githubusercontent.com
+GITHUB_USERNAME := rduplain
 
-export VENDOR
+export REQD_DIR := $(PROJECT_ROOT)/.reqd
 
-$(nixd): $(__FILE__)
+reqd := $(REQD_DIR)/bin/reqd
+
+$(reqd): $(__FILE__) | curl-command
 	@rm -f $@
-	@$(VENDOR)/bin/nixd-bootstrap $(nixd) $(NIXD_VERSION) sha256 $(NIXD_SHA256)
+	@curl -sSL qwerty.sh |\
+		sh -s - \
+		--sha256=$(REQD_SHA256) \
+		--output=$@ --chmod=a+x \
+		https://$(GITHUB_RAW)/$(GITHUB_USERNAME)/reqd/$(REQD_VERSION)/bin/reqd
 
-vendor-%: $(nixd)
-	@$(nixd) install $*
+reqd-%: $(reqd)
+	@$(reqd) install $*
