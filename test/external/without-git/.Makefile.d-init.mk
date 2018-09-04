a30fa719533e047baf87cb227215523066ecfa98
Ron DuPlain <ron.duplain@gmail.com>
2018-09-03 23:13:17 -0400

    Define full qwerty.sh command up front.

--- test/external/without-git/.Makefile.d-init.mk
+++ test/external/without-git/.Makefile.d-init.mk
@@ -1,19 +1,16 @@
 include .Makefile.d/path.mk
 
-# See Makefile.d project README to compute SHA256.
-MAKEFILE_D_VERSION := e2949bc
-MAKEFILE_D_SHA256 := \
-	6c75a7f055b49e2a53d8f44811a928351dbd1446ac4ae1d5bf828598a097dfd3
-MAKEFILE_D_URL := \
-	https://github.com/rduplain/Makefile.d/tarball/$(MAKEFILE_D_VERSION)
+define qwerty-Makefile.d
+curl -sSL qwerty.sh |\
+	sh -s - \
+	--sha256=6c75a7f055b49e2a53d8f44811a928351dbd1446ac4ae1d5bf828598a097dfd3 \
+	https://github.com/rduplain/Makefile.d/tarball/e2949bc |\
+			tar -xvzf - --strip-components=1
+endef
 
 .Makefile.d/%.mk:
 	@mkdir -p .Makefile.d
-	cd .Makefile.d; curl -sSL qwerty.sh |\
-		sh -s - \
-		--sha256=$(MAKEFILE_D_SHA256) \
-		$(MAKEFILE_D_URL) |\
-			tar -xvzf - --strip-components=1
+	cd .Makefile.d; $(qwerty-Makefile.d)
 
 prove-Makefile.d:
 	@echo $(PROJECT_ROOT)
