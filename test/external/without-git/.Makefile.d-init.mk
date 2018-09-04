8815979988b37849e317ca130bb98f11902ba2c0
Ron DuPlain <ron.duplain@gmail.com>
2018-09-03 23:20:57 -0400

    Remove spurious tab stop.

--- test/external/without-git/.Makefile.d-init.mk
+++ test/external/without-git/.Makefile.d-init.mk
@@ -5,7 +5,7 @@ curl -sSL qwerty.sh |\
 	sh -s - \
 	--sha256=b3101c600ebaacd95bdee37eafe81394329b19b73d04d3a0d7a079e9f1074cca \
 	https://github.com/rduplain/Makefile.d/tarball/a30fa71 |\
-			tar -xvzf - --strip-components=1
+		tar -xvzf - --strip-components=1
 endef
 
 .Makefile.d/%.mk:
