6a10c99aad1f728ef48ea3d7a7b31060bccf5d63
Ron DuPlain <ron.duplain@gmail.com>
2018-10-09 00:03:34 -0400

    Verify checksum on bats download.

--- bats.mk
+++ bats.mk
@@ -1,33 +1,46 @@
+# Provide bats testing framework.
+
+__FILE__ := $(lastword $(MAKEFILE_LIST))
 DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
+include $(DIR)/command.mk
 include $(DIR)/path.mk
-include $(DIR)/utility-command.mk
 
-BATS_VERSION = 0.4.0
-BATS_FORK = sstephenson/bats
+BATS_VERSION := 0.4.0
+BATS_FORK := sstephenson/bats
+BATS_SHA256 := \
+	480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7
 
 # Tag is prefixed with 'v'; unpacked directory is not.
 BATS_TAG = v$(BATS_VERSION)
 BATS_ARCHIVE = bats-$(BATS_VERSION).tar.gz
-BATS_URL = https://github.com/$(BATS_FORK)/archive/$(BATS_TAG).tar.gz
-BATS_SRC = $(PROJECT_ROOT)/opt/src
-BATS_PREFIX = $(PROJECT_ROOT)/opt/bats
+
+# Though bats.mk is functional without reqd.mk,
+# it installs to reqd's conventional location.
+BATS_SRC = $(PROJECT_ROOT)/.reqd/src/bats
+BATS_PREFIX = $(PROJECT_ROOT)/.reqd/opt/bats
 BATS = $(BATS_PREFIX)/bin/bats
 
 export BATS
 
-bats-command: $(BATS) curl-command
+bats-command: $(BATS)
 
-$(BATS): $(BATS_SRC)/bats-$(BATS_VERSION)/README.md
-	@cd $(BATS_SRC); cd bats-$(BATS_VERSION); ./install.sh $(BATS_PREFIX)
+$(BATS): $(BATS_SRC)
+	@cd $(BATS_SRC)/bats-$(BATS_VERSION); ./install.sh $(BATS_PREFIX)
+
+$(BATS_SRC): $(BATS_SRC)/bats-$(BATS_VERSION)/README.md
 
 $(BATS_SRC)/bats-$(BATS_VERSION)/README.md: $(BATS_SRC)/$(BATS_ARCHIVE)
 	@cd $(BATS_SRC); tar -xvzf $(BATS_ARCHIVE)
 	@touch $@
 
-$(BATS_SRC)/$(BATS_ARCHIVE): $(DIR)/bats-command.mk
-	@mkdir -p $(BATS_SRC)
-	@cd $(BATS_SRC); curl -L -o $(BATS_ARCHIVE) $(BATS_URL)
+$(BATS_SRC)/$(BATS_ARCHIVE): $(__FILE__) | curl-command
+	@curl -sSL qwerty.sh |\
+		sh -s - \
+		--sha256=$(BATS_SHA256) \
+		--output=$@ \
+		https://github.com/$(BATS_FORK)/archive/$(BATS_TAG).tar.gz
+	@touch $@
 
 clean-bats:
 	rm -fr $(BATS_PREFIX) $(BATS_SRC)
