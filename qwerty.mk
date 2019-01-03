75b29d560b5d2b4441f24dd2daca9d5cf234aab0
Ron DuPlain <ron.duplain@gmail.com>
2019-01-03 04:14:38 -0500

    Provide qwerty.sh command.

---
+++ qwerty.mk
@@ -0,0 +1,9 @@
+# Provide qwerty.sh command via $(QWERTY_SH).
+
+DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(DIR)/command.mk
+
+ifeq ($(QWERTY_SH),)
+QWERTY_SH := curl -sSL qwerty.sh | sh -s -
+endif
