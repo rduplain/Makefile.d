d36f4f0f68a564b7349aa1822bd535443a6faec3
Ron DuPlain <ron.duplain@gmail.com>
2019-01-03 06:15:06 -0500

    Release v1.0.

--- .Makefile.d-init.mk
+++ .Makefile.d-init.mk
@@ -1,7 +1,7 @@
 __FILE__ := $(abspath $(lastword $(MAKEFILE_LIST)))
 
 MAKEFILE_D_URL := https://github.com/rduplain/Makefile.d.git
-MAKEFILE_D_REV := d9c21e7
+MAKEFILE_D_REV := v1.0
 
 ifeq ($(QWERTY_SH),)
 QWERTY_SH := curl -sSL qwerty.sh | sh -s -
