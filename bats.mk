ee19e8380589f3848d89c4d0f9497f4d036b2355
Ron DuPlain <ron.duplain@gmail.com>
2019-01-03 04:14:47 -0500

    Simplify bats command using qwerty.

--- bats.mk
+++ bats.mk
@@ -3,47 +3,21 @@
 __FILE__ := $(lastword $(MAKEFILE_LIST))
 DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-include $(DIR)/command.mk
+BATS_URL := https://github.com/sstephenson/bats.git
+BATS_REV := v0.4.0
+
 include $(DIR)/path.mk
+include $(DIR)/qwerty.mk
 
-BATS_VERSION := 0.4.0
-BATS_FORK := sstephenson/bats
-BATS_SHA256 := \
-	480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7
-
-# Tag is prefixed with 'v'; unpacked directory is not.
-BATS_TAG = v$(BATS_VERSION)
-BATS_ARCHIVE = bats-$(BATS_VERSION).tar.gz
-
-# Though bats.mk is functional without reqd.mk,
-# it installs to reqd's conventional location.
-BATS_SRC = $(PROJECT_ROOT)/.reqd/src/bats
-BATS_UNPACKED_PATH = $(BATS_SRC)/bats-$(BATS_VERSION)
+# reqd is not required, but use its conventional location.
+BATS_SRC = $(PROJECT_ROOT)/.reqd/src/bats/$(BATS_REV)
 BATS_PREFIX = $(PROJECT_ROOT)/.reqd/opt/bats
 BATS = $(BATS_PREFIX)/bin/bats
 
-export BATS
-
 bats-command: $(BATS)
+	@true
 
-$(BATS): $(BATS_SRC)
-	@cd $(BATS_SRC)/bats-$(BATS_VERSION); ./install.sh $(BATS_PREFIX)
-
-$(BATS_SRC): $(BATS_UNPACKED_PATH)/README.md
-
-$(BATS_UNPACKED_PATH)/README.md: $(BATS_SRC)/$(BATS_ARCHIVE)
-	@cd $(BATS_SRC); tar -xvzf $(BATS_ARCHIVE)
-	@touch $@
-
-$(BATS_SRC)/$(BATS_ARCHIVE): $(__FILE__) | curl-command
-	@curl -sSL qwerty.sh |\
-		sh -s - \
-		--sha256=$(BATS_SHA256) \
-		--output=$@ \
-		https://github.com/$(BATS_FORK)/archive/$(BATS_TAG).tar.gz
-	@touch $@
-
-clean-bats:
-	rm -fr $(BATS_PREFIX) $(BATS_SRC)
-
-.PHONY: bats-command
+$(BATS): $(__FILE__)
+	$(QWERTY_SH) -f -o $(BATS_SRC) --tag $(BATS_REV) $(BATS_URL)
+	cd $(BATS_SRC); ./install.sh $(BATS_PREFIX)
+	touch $@
