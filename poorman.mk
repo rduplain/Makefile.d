86b27f600297e06d6f3d4aa0ce39c2b226ba9305
Ron DuPlain <ron.duplain@gmail.com>
2019-01-03 04:55:52 -0500

    Update poorman style.

--- poorman.mk
+++ poorman.mk
@@ -3,21 +3,18 @@
 __FILE__ := $(lastword $(MAKEFILE_LIST))
 DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-include $(DIR)/command.mk
-include $(DIR)/path.mk
-
 POORMAN_URL := https://github.com/rduplain/poorman.git
 POORMAN_REV := v0.6.2
 
-# Though poorman.mk is functional without reqd.mk,
-# it installs to reqd's conventional location.
+include $(DIR)/path.mk
+include $(DIR)/qwerty.mk
+
+# reqd is not required, but use its conventional location.
 POORMAN := $(PROJECT_ROOT)/.reqd/usr/bin/poorman
 
-$(POORMAN): $(__FILE__) | curl-command
-	@rm -f $@
-	@curl -sSL qwerty.sh | sh -s - \
-		--tag $(POORMAN_REV) --chmod=a+x $(POORMAN_URL) poorman:$@
-
-# Replace command.mk's `poorman-command` to download poorman.
 poorman-command: $(POORMAN)
 	@true
+
+$(POORMAN): $(__FILE__)
+	@rm -f $@
+	$(QWERTY_SH) --tag $(POORMAN_REV) --chmod=a+x $(POORMAN_URL) poorman:$@
