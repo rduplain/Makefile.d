b6848f4a1277ca8cd7fd736df0e7418710cf7df1
Rom DuPlain <rom.duplain@gmail.com>
2019-10-13 00:51:13 -0700

    Speed up Makefile.d download, by default.
    This applies only to projects which use this updated init .mk file.

--- .Makefile.d-init.mk
+++ .Makefile.d-init.mk
@@ -1,7 +1,7 @@
 __FILE__ := $(abspath $(lastword $(MAKEFILE_LIST)))
 
 MAKEFILE_D_URL := https://github.com/rduplain/Makefile.d.git
-MAKEFILE_D_REV := v1.1
+MAKEFILE_D_REV := v1.1 # Use --ref instead of --tag below if untagged.
 
 ifeq ($(QWERTY_SH),)
 QWERTY_SH := curl -sSL qwerty.sh | sh -s -
@@ -11,5 +11,5 @@ endif
 	@touch $@
 
 .Makefile.d/path.mk: $(__FILE__)
-	$(QWERTY_SH) -f -o .Makefile.d --ref $(MAKEFILE_D_REV) $(MAKEFILE_D_URL)
+	$(QWERTY_SH) -f -o .Makefile.d --tag $(MAKEFILE_D_REV) $(MAKEFILE_D_URL)
 	@touch $@
