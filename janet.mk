d23136ac035003adf058702217f44ec0eb698433
Rom DuPlain <rom.duplain@gmail.com>
2019-10-13 00:08:32 -0700

    Provide a full project-local Janet installation.

---
+++ janet.mk
@@ -0,0 +1,35 @@
+# Provide a full project-local Janet installation.
+
+__FILE__ := $(lastword $(MAKEFILE_LIST))
+DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
+
+JANET_URL ?= https://github.com/janet-lang/janet.git
+JANET_REV ?= v1.3.1
+
+include $(DIR)/path.mk
+include $(DIR)/qwerty.mk
+
+# reqd is not required, but use its conventional location.
+JANET_SRC = $(PROJECT_ROOT)/.reqd/src/janet/$(JANET_REV)
+JANET_PREFIX = $(PROJECT_ROOT)/.reqd/opt/janet
+JANET = $(JANET_PREFIX)/bin/janet
+JPM = $(JANET_PREFIX)/bin/jpm
+
+export PATH := $(JANET_PREFIX)/bin:$(PATH)
+
+janet-command: $(JANET)
+	@true
+
+jpm-command: $(JANET)
+	@true
+
+$(JANET): $(__FILE__)
+	test -d $(JANET_SRC) || git clone $(JANET_URL) $(JANET_SRC)
+	cd $(JANET_SRC); git checkout $(JANET_REV)
+	cd $(JANET_SRC); make && make install
+	@echo "Janet executable in place: $(JANET) ..."
+	touch $@
+
+$(JANET): export PREFIX = $(JANET_PREFIX)
+$(JANET): export MANPATH = $(PREFIX)/share/man/man1/
+$(JANET): export PKG_CONFIG_PATH = $(PREFIX)/lib/pkgconfig
