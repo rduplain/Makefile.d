215ed244a6bf4c02d5c6a9ac9fc0c57d5d0c6059
Rom DuPlain <rom.duplain@gmail.com>
2019-11-07 20:46:53 -0800

    Update Janet version.

--- janet.mk
+++ janet.mk
@@ -4,7 +4,7 @@ JANET_MK := $(lastword $(MAKEFILE_LIST))
 DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 JANET_URL ?= https://github.com/janet-lang/janet.git
-JANET_REV ?= v1.3.1
+JANET_REV ?= v1.4.0
 
 include $(DIR)/path.mk
 
