78107a7ebf4f3541b76edf436ff82c6d891c150a
Rom DuPlain <rom.duplain@gmail.com>
2020-01-07 11:51:00 -0800

    Cache additional build directory.

--- .travis.yml.bash
+++ .travis.yml.bash
@@ -132,7 +132,8 @@ osx                                                            $os && cat <<___
 ___
 all                                                            $os && cat <<___
           - ./test/python/.reqd/src
-          - ./test/misc/.reqd/
+          - ./test/misc/.reqd
+          - ./test/misc-alt/.reqd
 ___
 osx                                                            $os && cat <<___
           - ~/Library/Caches/Homebrew
