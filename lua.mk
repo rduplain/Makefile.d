8d964a5ddd65d6b6e1cfd98ab5de8f5fc5f28811
Rom DuPlain <rom.duplain@gmail.com>
2020-01-06 23:20:59 -0800

    Split LuaRocks into its own .mk include.

--- lua.mk
+++ lua.mk
@@ -1,15 +1,8 @@
 # Provide a project-local Lua installation.
 #
-# Lua: Without an OS-specific patch, Lua does not support dynamic libraries
-# (.so) when using `make posix`. This limits use of many luarocks-installable
+# Without an OS-specific patch, Lua does not support dynamic libraries (.so)
+# when using `make posix`. This limits use of many luarocks-installable
 # packages, including `luacheck`.
-#
-# LuaRocks: This .mk builds project-local binaries for `lua` and `luarocks` at
-# a relative directory. Accordingly, `luarocks` will install its system/global
-# tree to a project-local directory. By convention, given this behavior,
-# projects using this .mk include can use unqualified `luarocks` installation
-# commands (and not --local features of `luarocks`) so that resulting packages
-# install to a project-local directory.
 
 LUA_MK := $(lastword $(MAKEFILE_LIST))
 DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
@@ -19,26 +12,18 @@ LUA_REV ?= 5.3.5
 LUA_URL ?= $(LUA_BASE_URL)/lua-$(LUA_REV).tar.gz
 LUA_SHA256 ?= 0c2eed3f960446e1a3e4b9a1ca2f3ff893b6ce41942cf54d5dd59ab4b3b058ac
 
-LUAROCKS_BASE_URL ?= https://luarocks.org/releases
-LUAROCKS_REV ?= 3.2.1
-LUAROCKS_URL ?= $(LUAROCKS_BASE_URL)/luarocks-$(LUAROCKS_REV).tar.gz
-LUAROCKS_SHA256 ?= \
-	f27e20c9cdb3ffb991ccdb85796c36a0690566676f8e1a59b0d0ee6598907d04
-
 include $(DIR)/path.mk
 include $(DIR)/qwerty.mk
 
 LUA_SRC = $(PROJECT_ROOT)/.reqd/src/lua/$(LUA_REV)
 LUA_PREFIX = $(PROJECT_ROOT)/.reqd/opt/lua
 LUA = $(LUA_PREFIX)/bin/lua
-LUAROCKS = $(LUA_PREFIX)/bin/luarocks
+
+include $(DIR)/luarocks.mk
 
 export PATH := $(LUA_PREFIX)/bin:$(PATH)
 
 lua-command: $(LUA)
-	@true
-
-luarocks-command: $(LUAROCKS)
 	@true
 
 $(LUA): $(LUA_SRC)/lua-$(LUA_REV).tar.gz $(LUA_MK)
@@ -56,17 +41,3 @@ $(LUA): $(LUA_SRC)/lua-$(LUA_REV).tar.gz $(LUA_MK)
 
 $(LUA_SRC)/lua-$(LUA_REV).tar.gz:
 	$(QWERTY_SH) --sha256=$(LUA_SHA256) --output=$@ $(LUA_URL)
-
-$(LUAROCKS): $(LUA_SRC)/luarocks-$(LUAROCKS_REV).tar.gz $(LUA)
-	cd $(LUA_SRC); tar -xf luarocks-$(LUAROCKS_REV).tar.gz
-
-	cd $(LUA_SRC)/luarocks-$(LUAROCKS_REV); \
-		./configure --prefix=$(LUA_PREFIX) --with-lua=$(LUA_PREFIX) && \
-		$(MAKE) $(LUAROCKS_BUILD_FLAGS) && \
-		$(MAKE) install
-
-	@echo "LuaRocks executable in place: $(LUAROCKS) ..."
-	@touch $@
-
-$(LUA_SRC)/luarocks-$(LUAROCKS_REV).tar.gz:
-	$(QWERTY_SH) --sha256=$(LUAROCKS_SHA256) --output=$@ $(LUAROCKS_URL)
---
+++ luarocks.mk
@@ -0,0 +1,40 @@
+# Provide LuaRocks within project-local Lua installation.
+#
+# This .mk is not intended for direct inclusion in user projects. It depends on
+# variables set by lua.mk and is included automatically by lua.mk.
+#
+# Makefile.d builds project-local binaries for `lua` and `luarocks` at a
+# relative directory. Accordingly, `luarocks` will install its system/global
+# tree to a project-local directory. By convention, given this behavior,
+# projects using this .mk include can use unqualified `luarocks` installation
+# commands (and not --local features of `luarocks`) so that resulting packages
+# install to a project-local directory.
+
+DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
+
+LUAROCKS_BASE_URL ?= https://luarocks.org/releases
+LUAROCKS_REV ?= 3.2.1
+LUAROCKS_URL ?= $(LUAROCKS_BASE_URL)/luarocks-$(LUAROCKS_REV).tar.gz
+LUAROCKS_SHA256 ?= \
+	f27e20c9cdb3ffb991ccdb85796c36a0690566676f8e1a59b0d0ee6598907d04
+
+include $(DIR)/qwerty.mk
+
+LUAROCKS = $(LUA_PREFIX)/bin/luarocks
+
+luarocks-command: $(LUAROCKS)
+	@true
+
+$(LUAROCKS): $(LUA_SRC)/luarocks-$(LUAROCKS_REV).tar.gz $(LUA)
+	cd $(LUA_SRC); tar -xf luarocks-$(LUAROCKS_REV).tar.gz
+
+	cd $(LUA_SRC)/luarocks-$(LUAROCKS_REV); \
+		./configure --prefix=$(LUA_PREFIX) --with-lua=$(LUA_PREFIX) && \
+		$(MAKE) $(LUAROCKS_BUILD_FLAGS) && \
+		$(MAKE) install
+
+	@echo "LuaRocks executable in place: $(LUAROCKS) ..."
+	@touch $@
+
+$(LUA_SRC)/luarocks-$(LUAROCKS_REV).tar.gz:
+	$(QWERTY_SH) --sha256=$(LUAROCKS_SHA256) --output=$@ $(LUAROCKS_URL)
