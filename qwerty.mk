ad64d068d0d1b119f298b8868f6c71257dabfb7e
Rom DuPlain <rom.duplain@gmail.com>
2020-01-16 17:58:10 -0800

    Simplify QWERTY_SH & QWERTY_SH_URL initialization.

--- qwerty.mk
+++ qwerty.mk
@@ -4,10 +4,5 @@ DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 include $(DIR)/command.mk
 
-ifeq ($(QWERTY_SH_URL),)
-QWERTY_SH_URL := https://qwerty.sh
-endif
-
-ifeq ($(QWERTY_SH),)
-QWERTY_SH := curl --proto '=https' --tlsv1.2 -sSf $(QWERTY_SH_URL) | sh -s -
-endif
+QWERTY_SH_URL ?= https://qwerty.sh
+QWERTY_SH ?= curl --proto '=https' --tlsv1.2 -sSf $(QWERTY_SH_URL) | sh -s -
