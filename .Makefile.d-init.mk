992e0d305a880b24962fe3baa5f195246f249405
Rom DuPlain <rom.duplain@gmail.com>
2020-03-09 12:21:55 -0700

    Release v1.4.3.

--- .Makefile.d-init.mk
+++ .Makefile.d-init.mk
@@ -1,7 +1,7 @@
 MAKEFILE_D_INIT_MK := $(abspath $(lastword $(MAKEFILE_LIST)))
 
 MAKEFILE_D_URL ?= https://github.com/rduplain/Makefile.d.git
-MAKEFILE_D_REV ?= v1.4.2 # Use --ref instead of --tag below if untagged.
+MAKEFILE_D_REV ?= v1.4.3 # Use --ref instead of --tag below if untagged.
 
 QWERTY_SH_URL ?= https://qwerty.sh
 QWERTY_SH ?= curl --proto '=https' --tlsv1.2 -sSf $(QWERTY_SH_URL) | sh -s -
