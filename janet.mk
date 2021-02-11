9f16d6ac275fa1571658806ed21cb397de937ff1
Rom DuPlain <rom.duplain@gmail.com>
2021-02-10 22:25:09 -0800

    Use Janet v1.15.0.

--- janet.mk
+++ janet.mk
@@ -4,7 +4,7 @@ JANET_MK := $(lastword $(MAKEFILE_LIST))
 DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 
 JANET_URL ?= https://github.com/janet-lang/janet.git
-JANET_REV ?= v1.13.1
+JANET_REV ?= v1.15.0
 
 include $(DIR)/path.mk
 
